3857c5efdf9ff2196e13476b99c44699c040b338
Ingrydgoncalves <ingryd-lorrene@hotmail.com>
2025-06-13 16:23:46 -0300

    Improve context

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generaterecipe (event) {
     let recipediv = document.querySelector("#recipe");
     let apikey = "ce024dff2t11ac0410oc03803af4bc0a";
     let prompt= `User instructions: Generate a recipe with ${instructions.value}`;
-    let context= `Generate a simple recipe with ingrident of instruction. Your mission is make a recipe in basic html, Separate the line with </br>. Make maxim 10 lines.  And dont show the  symbol  html in page. All text is must to be in black color . Follow  all instructions.`;
+    let context = `Generate a simple recipe with ingrident of instruction. Your mission is make a recipe in basic html, Separate the line with </br>. Make maxim 10 lines.  And dont show the  symbol  html in page. Start the recipe with title inside <div style=rgb(15, 4, 1); font-size:42px; font-family:Georgia, 'Times New Roman', Times, serif;> .Follow all instructions.`;
     let apiurl= `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`
 
 let recipeelement=document.querySelector("#recipe");
